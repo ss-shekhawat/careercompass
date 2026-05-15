5cb50cd72b69175476797b00d6514679187ba040
ss-shekhawat <ssshekhawat0896@gmail.com>
2026-05-15 22:48:52 +0530

    Delete src/pages copy directory

--- src/pages copy/student/ResumeAssessment.jsx
+++
@@ -1,7 +0,0 @@
-
-// export default ResumeAssessment;
-import AssessmentWrapper from "../../components/AssessmentWrapper";
-
-const ResumeAssessment = () => <AssessmentWrapper isResume={true} />;
-
-export default ResumeAssessment;
